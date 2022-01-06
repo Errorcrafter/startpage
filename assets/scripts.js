ab605ec65a4029c93808518dfb5c796928de9c54
g1ga mcskidsalot <69189354+Errorcrafter@users.noreply.github.com>
2022-01-06 17:04:50 +0800

    update

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,21 +4,25 @@ function getTime() {
         sec = date.getSeconds(),
         hour = date.getHours();
 
-    return (
-        "" +
-        (hour < 10 ? "0" + hour : hour) +
-        ":" +
-        (min < 10 ? "0" + min : min) +
-        ":" +
-        (sec < 10 ? "0" + sec : sec)
-    );
+    if (hour >= 22 || hour <= 5) {
+        return "go the fuck to sleep";
+    } else {
+        return (
+            "" +
+            (hour < 10 ? "0" + hour : hour) +
+            ":" +
+            (min < 10 ? "0" + min : min) +
+            ":" +
+            (sec < 10 ? "0" + sec : sec)
+        );
+    }
 }
 
 function getMOTD() {
-    var motds = Array("hello world!", "what does static do java", "spigotmc", "the sun corp", "google dot com",
+    var motds = ["hello world!", "what does static do java", "spigotmc", "the sun corp", "google dot com",
         "idiots being stupid", "youtube", "bing", "how to unstatic all fields intellij", "coding with swastik",
         "pics of potatoes","snippyware","sigma deleter","qqbackdoor","github","red it","VAPE V4 CRACK (NO VIRUS WORKING 2021)",
-        "tokenlogger example code","wikipedia","test","pihack","meteor client","wurst","p","w+3")
+        "tokenlogger example code","wikipedia","test","pihack","meteor client","wurst","p","w+3"]
     return motds[Math.floor(Math.random() * motds.length)];;
 }
 
@@ -30,25 +34,16 @@ function search(e) {
 }
 
 function loadMenus(x) {
-    var mainMenu = document.getElementById("table-main");      // 0
-    var schMenu = document.getElementById("table-sch");        // 1
-    var clientMenu = document.getElementById("table-games");   // 2
+    var menuList = [document.getElementById("table-main"),
+                    document.getElementById("table-sch"),
+                    document.getElementById("table-games")]
 
-    var dispShow = "margin-left: auto; margin-right: auto; display: unset;"
-    var dispHide = "margin-left: auto; margin-right: auto; display: none;"
-
-    if (x==0) {
-        mainMenu.style = dispShow;
-        schMenu.style = dispHide;
-        clientMenu.style = dispHide;
-    } else if (x==1) {
-        mainMenu.style = dispHide;
-        schMenu.style = dispShow;
-        clientMenu.style = dispHide;
-    } else if (x==2) {
-        mainMenu.style = dispHide;
-        schMenu.style = dispHide;
-        clientMenu.style = dispShow;
+    for (let i = 0; i <= menuList.length; i++) {
+        if (i == x) {
+            menuList[i].style = "margin-left: auto; margin-right: auto; display: unset;"
+        } else {
+            menuList[i].style = "margin-left: auto; margin-right: auto; display: none;"
+        }
     }
 }
 
